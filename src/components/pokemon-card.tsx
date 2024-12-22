59b238ca65b74e2b12355b28b4d997b277338fd9
Gautier BOUTROY <gautier.boutroy@2022.icam.fr>
2024-12-22 17:49:09 +0100

    Ajout d'un Props : quand l'utilisateur passe sa souris sur une carte dans la page pokémon list de ma WebApp, la carte du pokémon change de couleur

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
   
@@ -8,10 +8,19 @@ type Props = {
 };
   
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
-    
+  const [color, setColor] = useState<string>(); // Etat pour stocker la couleur actuel de la bordure
+
+  const showBorder = () => {
+    setColor(borderColor); // Quand les conditions de showBorder sont réunis, couleur par défaut #009688
+  }
+
+  const hideBorder = () => {
+    setColor('#f5f5f5'); // Quand les conditions de hideBorder sont réunis, couleur grise
+  }
+  
   return (
-    <div className="col s6 m4">
-      <div className="card horizontal" style={{borderColor : borderColor}}>
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style={{borderColor : color}}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
