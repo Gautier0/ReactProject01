8719f0b421ac7ca44739c10fbcc32a17e945814d
Gautier BOUTROY <gautier.boutroy@2022.icam.fr>
2024-12-22 16:55:44 +0100

    update 22 dec 2022

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,16 +1,29 @@
-import React, { FunctionComponent }from "react";
-import Pokemon from "../models/pokemon";
-
+import React, { FunctionComponent } from 'react';
+import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+  
 type Props = {
-    pokemon: Pokemon
+  pokemon: Pokemon
+  borderColor?: string
 };
-
-const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
-    return (
-        <div>
-            Ce composant est chargé d'afficher le pokémon : {pokemon.name}
+  
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
+    
+  return (
+    <div className="col s6 m4">
+      <div className="card horizontal" style={{borderColor : borderColor}}>
+        <div className="card-image"> 
+          <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
-    );
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
 }
-
+  
 export default PokemonCard;
